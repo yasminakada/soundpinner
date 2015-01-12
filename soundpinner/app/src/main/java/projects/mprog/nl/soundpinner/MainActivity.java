cc5601660db5eaf88923e637b96b4c822991fbfc
yasminakada <y.kada@live.nl>
2015-01-12 11:26:27 +0100

    added soundrec as new project

--- soundpinner/app/src/main/java/projects/mprog/nl/soundpinner/MainActivity.java
+++ soundpinner/app/src/main/java/projects/mprog/nl/soundpinner/MainActivity.java
@@ -1,6 +1,9 @@
 package projects.mprog.nl.soundpinner;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,7 +14,41 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        // Allows you to interact with Fragments in an Activity
+        FragmentManager fragmentManager = getFragmentManager();
+
+        // beginTransaction() begins the FragmentTransaction which allows you to
+        // add, attach, detach, hide, remove, replace, animate, transition or
+        // show fragments
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+
+        // The Configuration object provides device configuration info
+        // http://developer.android.com/reference/android/content/res/Configuration.html
+        Configuration configInfo = getResources().getConfiguration();
+
+        // Depending on the screen orientation replace with the correct fragment
+        if(configInfo.orientation == Configuration.ORIENTATION_LANDSCAPE){
+
+            FragmentLandscape fragmentLandscape = new FragmentLandscape();
+
+            fragmentTransaction.replace(android.R.id.content,
+                    fragmentLandscape);
+
+        } else {
+
+            FragmentPortrait fragmentPortrait = new FragmentPortrait();
+
+            fragmentTransaction.replace(android.R.id.content,
+                    fragmentPortrait);
+
+        }
+
+        // Schedule for the replacement of the Fragment as soon as possible
+        fragmentTransaction.commit();
+
+        // setContentView(R.layout.activity_my);
     }
 
 
