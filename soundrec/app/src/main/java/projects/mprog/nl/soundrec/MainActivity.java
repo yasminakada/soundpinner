e1a418e17f8202e74f74f44c2be57072093c3541
yasminakada <y.kada@live.nl>
2015-01-16 21:39:09 +0100

    Working on making interface somewhat pretty

--- soundrec/app/src/main/java/projects/mprog/nl/soundrec/MainActivity.java
+++ soundrec/app/src/main/java/projects/mprog/nl/soundrec/MainActivity.java
@@ -1,9 +1,15 @@
 package projects.mprog.nl.soundrec;
 
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Display;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 /**
  * Yasmina Kada
@@ -11,15 +17,40 @@ import android.support.v4.app.FragmentActivity;
  * 10001567
  */
 public class MainActivity extends FragmentActivity{
+    int scrWidth = 0;
+    int scrHeight = 0;
+    TabsFragment tabsFragment;
+    RecordFragment recordFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabsFragment tabsFragment = new TabsFragment();
-        RecordFragment recordFragment = new RecordFragment();
+        tabsFragment = new TabsFragment();
+        recordFragment = new RecordFragment();
 
+        setScreenDimensions();
+        setFragmentContainersDimensions();
+        setFragmentsView();
+        FileConstruct.createMainDirectory();
+    }
+    public void setScreenDimensions() {
+        // Getting the screen size of the device
+        if (Build.VERSION.SDK_INT >= 13) {
+            Display display = getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            scrWidth = size.x;
+            scrHeight = size.y;
+        } else {
+            Display display = getWindowManager().getDefaultDisplay();
+            scrWidth = display.getWidth();
+            scrHeight = display.getHeight();
+        }
+    }
+
+    public void setFragmentsView(){
         FragmentManager manager = getSupportFragmentManager();
         tabsFragment.setManager(manager);
         FragmentTransaction ft = manager.beginTransaction();
@@ -27,9 +58,20 @@ public class MainActivity extends FragmentActivity{
         ft.add(R.id.fragment_container_bottom, tabsFragment);
 
         ft.commit();
+    }
 
-        FileConstruct.getDate();
-        FileConstruct.createMainDirectory();
+    public void setFragmentContainersDimensions(){
+        if (scrHeight > 0) {
+            FrameLayout topContainer = (FrameLayout) findViewById(R.id.fragment_container_top);
+            FrameLayout bottomContainer = (FrameLayout) findViewById(R.id.fragment_container_bottom);
+
+            ViewGroup.LayoutParams params = topContainer.getLayoutParams();
+            params.height =(scrHeight / 5) * 4;
+
+            params = bottomContainer.getLayoutParams();
+            params.height =(scrHeight / 5);
+
+        }
     }
 
 }
---
+++ soundrec/app/src/main/java/projects/mprog/nl/soundrec/RecordTabFragment.java
@@ -0,0 +1,21 @@
+package projects.mprog.nl.soundrec;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Yasmina Kada
+ * Programming Project 2015
+ * 10001567
+ */
+public class RecordTabFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_record, container, false);
+    }
+}
